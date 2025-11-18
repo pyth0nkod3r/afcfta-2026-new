0695d1e91c0f3260c6a46b0667d0d65de2f22766
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2025-11-18 21:39:07 +0100

    fix: vercel eror

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,16 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { fileURLToPath, URL } from "url";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   resolve: {
     alias: {
-      "~": fileURLToPath(new URL("./app", import.meta.url)),
+      "~": path.resolve(__dirname, "./app"),
     },
   },
 });
