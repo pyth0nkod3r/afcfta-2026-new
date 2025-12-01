3ce81c23aeb863d43001a0a469c265aaeca79240
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2025-12-01 14:38:22 +0100

    ssr fix

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,9 +3,9 @@ import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
   // Config options...
-  future: {
-    v8_middleware: true,
-  },
+  // future: {
+  //   v8_middleware: true,
+  // },
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
   presets: [vercelPreset()],
