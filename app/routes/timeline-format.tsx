89ccbc2a6dec348220198ad19cc69aac973c3534
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2025-12-01 10:53:41 +0100

    Integrate venue sections into timeline page

--- app/routes/timeline-format.tsx
+++ app/routes/timeline-format.tsx
@@ -4,6 +4,11 @@ import { HackathonDaysPhase } from "../components/timeline/HackathonDaysPhase";
 import { PostHackathonPhase } from "../components/timeline/PostHackathonPhase";
 import { FullScheduleSnapshot } from "../components/timeline/FullScheduleSnapshot";
 import { HybridFormatExplained } from "../components/timeline/HybridFormatExplained";
+import { VenueSection } from "../components/venue/VenueSection";
+import { VirtualParticipation } from "../components/venue/VirtualParticipation";
+import { WhyAbuja } from "../components/venue/WhyAbuja";
+import { EventFormatBreakdown } from "../components/venue/EventFormatBreakdown";
+import { AttendeeHighlights } from "../components/venue/AttendeeHighlights";
 import { TimelineCallToAction } from "../components/timeline/TimelineCallToAction";
 
 export default function TimelineFormat() {
@@ -15,6 +20,11 @@ export default function TimelineFormat() {
       <PostHackathonPhase />
       <FullScheduleSnapshot />
       <HybridFormatExplained />
+      <VenueSection />
+      <VirtualParticipation />
+      <WhyAbuja />
+      <EventFormatBreakdown />
+      <AttendeeHighlights />
       <TimelineCallToAction />
     </>
   );
